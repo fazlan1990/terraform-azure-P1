71a203a46eb153e1f0433d082b245c969b82da08
fazlan1990 <141962093+fazlan1990@users.noreply.github.com>
2024-10-15 14:25:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,4 +22,7 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_storage_account" "storage" {
   name                     = "faslanstr45"          # Storage account name must be lowercase, 3-24 characters
   location                 = azurerm_resource_group.rg.location
-  r
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
