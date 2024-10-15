df52e271476f0c9501c517b614931d7d95aed760
fazlan1990 <141962093+fazlan1990@users.noreply.github.com>
2024-10-15 16:31:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,3 +33,12 @@ resource "azurerm_storage_account" "storage" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
+resource "azurerm_storage_account" "storage1" {
+  name                     = "faslanstr4555"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
