19a5e6f7bba33769944d7c4d469045fc6376b64d
fazlan1990 <141962093+fazlan1990@users.noreply.github.com>
2024-10-24 06:03:17 +0530

    Update main.tf

--- 1-Creating_storage/main.tf
+++ 1-Creating_storage/main.tf
@@ -27,7 +27,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "faslanstr4536"
+  name                     = "faslanstr45136"
   location                 = azurerm_resource_group.rg.location
   resource_group_name      = azurerm_resource_group.rg.name
   account_tier             = "Standard"
